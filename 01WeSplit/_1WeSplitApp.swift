1c3d99e08c9d85a8356fca0c5f50b815ffdb11b8
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-01-16 12:55:02 -0800

    Initial Commit

---
+++ 01WeSplit/_1WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1WeSplitApp.swift
+//  01WeSplit
+//
+//  Created by Guy on 1/16/21.
+//
+
+import SwiftUI
+
+@main
+struct _1WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
